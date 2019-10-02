75ecb392eca2fd542dadfbf1066634896f325b72
Teto Gürel <tateguerel@gmail.com>
2019-10-02 19:14:36 +0200

    cleaned out file tree

---
+++ Assets/MyProject/Scripts/changeScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeScene : MonoBehaviour
+{
+
+    public void NextScene()
+    {
+        SceneManager.LoadScene("LvL_2", LoadSceneMode.Single);
+    }
+
+}
