cc4d34a845c05abd04658ac2e20468e63640f2bb
Teto Gürel <tateguerel@gmail.com>
2019-10-03 22:17:21 +0200

    cleaned names + applied feedback

--- Assets/MyProject/Scripts/changeScene.cs
+++ Assets/MyProject/Scripts/changeScene.cs
@@ -6,8 +6,8 @@ public class ChangeScene : MonoBehaviour
 
     public void NextScene()
     {   
-        //Loads Scene LvL_2, Single:Closes all other loaded scenes
-        SceneManager.LoadScene("LvL_2", LoadSceneMode.Single);
+        //Loads Scene LvL2, Single:Closes all other loaded scenes
+        SceneManager.LoadScene("LvL2", LoadSceneMode.Single);
     }
 
 }
